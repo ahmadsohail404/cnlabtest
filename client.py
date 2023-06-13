81b73700c2ad8ca37cfe3dfba00499b9fad077f3
Sohail Ahmad <sohailss2412@gmail.com>
2023-06-13 19:44:46 +0530

    .

--- client.py
+++ client.py
@@ -1,25 +1,19 @@
 import socket
 
-
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5001  # socket server port number
+    host = socket.gethostbyname()
+    port = 5001
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
-
-    message = input(" -> ")  # take input
+    message = input("->")
 
     while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
+        print("Received " + data)
+        message = input("->")
+    client_socket.close()
 
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     client_program()
