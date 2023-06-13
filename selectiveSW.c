cc99aa3b656975672b60f7a52c75c9a649828f13
Sohail Ahmad <sohailss2412@gmail.com>
2023-06-13 12:50:26 +0530

    lab progs

--- selectiveSW.c
+++ selectiveSW.c
@@ -1,31 +1,33 @@
-#include<stdio.h> #include<stdlib.h>
-void main() {
-int p,i=1,a,w,nac,ack,j=0;
-printf("Enter the frame length : "); 
-scanf("%d",&a);
-printf("Enter the window size : "); 
-scanf("%d",&w); 
-printf("\nTransmission started....\n\n"); 
-while(i<=a)
+#include <stdio.h> #include<stdlib.h>
+void main()
 {
-printf("Packets from %d to %d are transmitted\n",i,w+i-1); 
-for(p=i;p<w+i;p++)
-{
-printf("Transmitting packet %d\n",p); }
-nac=i+rand()%w; 
-if(nac==i)
-{
-printf("No Nack received\n");
-printf("All the packets in the current window are sent successfully!!!\n"); 
-i=i+w;
-continue;
-}
-printf("\nNack = %d\n\n",nac);
-printf("Ack received for all the packets except for packet %d\n",nac); 
-printf("Sending packet %d back\n\n",nac);
-printf("Ack is received for packet %d\n\n",nac);
-printf("All the packets in the current window are sent successfully!!!\n\n"); 
-i=i+w;
-}
-printf("USN : 4NI18IS022\n"); 
+  int p, i = 1, a, w, nac, ack, j = 0;
+  printf("Enter the frame length : ");
+  scanf("%d", &a);
+  printf("Enter the window size : ");
+  scanf("%d", &w);
+  printf("\nTransmission started....\n\n");
+  while (i <= a)
+  {
+    printf("Packets from %d to %d are transmitted\n", i, w + i - 1);
+    for (p = i; p < w + i; p++)
+    {
+      printf("Transmitting packet %d\n", p);
+    }
+    nac = i + rand() % w;
+    if (nac == i)
+    {
+      printf("No Nack received\n");
+      printf("All the packets in the current window are sent successfully!!!\n");
+      i = i + w;
+      continue;
+    }
+    printf("\nNack = %d\n\n", nac);
+    printf("Ack received for all the packets except for packet %d\n", nac);
+    printf("Sending packet %d back\n\n", nac);
+    printf("Ack is received for packet %d\n\n", nac);
+    printf("All the packets in the current window are sent successfully!!!\n\n");
+    i = i + w;
+  }
+  printf("USN : 4NI20IS108\n");
 }
--- slidingGoBack.c
+++ slidingGoBack.c
@@ -1,41 +1,41 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <time.h>
 
-void transmission(int i,int n,int tf)
+void transmission(int i, int n, int tf)
 {
         while (i <= tf)
         {
-                int z = 0,k;
+                int z = 0, k;
                 for (k = i; k < i + n && k <= tf; k++)
-                        printf("Sending frame %d\n",k);
+                        printf("Sending frame %d\n", k);
                 for (k = i; k < i + n && k <= tf; k++)
                 {
                         int f = rand() % 4;
                         if (!f)
                         {
-                                printf("Acknowledgement for frame %d\n",k);
+                                printf("Acknowledgement for frame %d\n", k);
                                 z++;
                         }
                         else
                         {
-                                printf("Frame %d not received\n",k);
+                                printf("Frame %d not received\n", k);
                                 printf("Retransmitting\n");
                                 break;
                         }
                 }
                 printf("\n");
-                i=i+z;
+                i = i + z;
         }
 }
 
 int main()
 {
-        int tf,n,i=1;
+        int tf, n, i = 1;
         srand(time(NULL));
         printf("Enter the total number of frames\n");
-        scanf("%d",&tf);
+        scanf("%d", &tf);
         printf("Enter the window size\n");
-        scanf("%d",&n);
-        transmission(i,n,tf);
+        scanf("%d", &n);
+        transmission(i, n, tf);
         return 0;
 }
